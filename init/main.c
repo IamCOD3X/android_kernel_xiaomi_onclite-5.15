218d522a6214c044ab1e183da529cb6b68e3436e
IamCOD3X <sourabh.panchal28@gmail.com>
2025-03-02 21:02:52 +0530

    init: Import Xiaomi changes Change-Id: I6608834242d6ad459bb4fce2190764638f517106

--- init/main.c
+++ init/main.c
@@ -3,6 +3,7 @@
  *  linux/init/main.c
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *  Copyright (C) 2020 XiaoMi, Inc.
  *
  *  GK 2/5/95  -  Changed to support mounting root fs via NFS
  *  Added initrd & change_root: Werner Almesberger & Hans Lermen, Feb '96
@@ -936,10 +937,13 @@ static void __init print_unknown_bootoptions(void)
 	memblock_free_ptr(unknown_options, len);
 }
 
+int fpsensor=1;
+
 asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 {
 	char *command_line;
 	char *after_dashes;
+	char *p=NULL;
 
 	set_task_stack_end_magic(&init_task);
 	smp_setup_processor_id();
@@ -971,6 +975,13 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	page_alloc_init();
 
 	pr_notice("Kernel command line: %s\n", saved_command_line);
+		p = NULL;
+	p= strstr(boot_command_line, "androidboot.fpsensor=fpc");
+	if(p) {
+		fpsensor = 1;//fpc fingerprint
+	} else {
+		fpsensor = 2;//goodix fingerprint
+	}	
 	/* parameters may set static keys */
 	jump_label_init();
 	parse_early_param();
